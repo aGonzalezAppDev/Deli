f4bc1f61d16a35a0fede9a5a58ba6f2dad4677df
Bmorse-coder <bmorse@appdev.yearup.org>
2023-06-01 13:17:38 -0400

    Updated drinks class

--- src/main/java/com/yearup/deli/Drink.java
+++ src/main/java/com/yearup/deli/Drink.java
@@ -1,12 +1,18 @@
 package com.yearup.deli;
 
+import java.util.ArrayList;
+
 public class Drink {
-
     private String size;
+    private String flavor;
+    private double price;
 
-    public Drink( String size) {
-
+    public Drink(String size, String flavor, double price) {
         this.size = size;
+        this.flavor = flavor;
+        this.price = price;
+
+
     }
 
     public String getSize() {
@@ -17,13 +23,40 @@ public class Drink {
         this.size = size;
     }
 
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
 
 
-    @Override
-    public String toString() {
-        return "Drink{" +
-                ", size='" + size + '\'' +
-                '}';
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    public StringBuilder printDrinkOrder() {
+        StringBuilder str = new StringBuilder();
+        str.append("Size: ").append(size).append("\n");
+        str.append("Flavor: ").append(flavor).append("\n");
+        str.append("Price: $").append(price);
+        return str;
+    }
+
+    // create a method for get price and drink size
+    public double getPrice() {
+        double drinkPrice = 0.0;
+        if (size.equalsIgnoreCase("small")) {
+            drinkPrice = 1.0;
+        } else if (size.equalsIgnoreCase("medium")) {
+            drinkPrice = 2.0;
+        } else if (size.equalsIgnoreCase("large")) {
+            drinkPrice = 3.0;
+
+        }
+        return drinkPrice;
     }
 }
 
