a7515538001831a9256e3f8aa01ade504aeac453
Bmorse-coder <bmorse@appdev.yearup.org>
2023-06-01 17:11:49 -0400

    Updated main

--- src/main/java/com/yearup/deli/Chips.java
+++ src/main/java/com/yearup/deli/Chips.java
@@ -3,8 +3,10 @@ package com.yearup.deli;
 public class Chips {
     private String chips;
 
+
     public Chips(String chips) {
         this.chips = chips;
+
     }
 
     public String getChips() {
@@ -14,4 +16,23 @@ public class Chips {
     public void setChips(String chips) {
         this.chips = chips;
     }
+
+
+
+    public StringBuilder printChipsOrder() {
+        StringBuilder str = new StringBuilder();
+        str.append("Chips: ").append(chips).append("\n");
+
+
+        return str;
+    }
+
+    public double getPrice() {
+    double chipPrice = 0.0;
+     return 1.5;
+
+    }
 }
+
+//get price return 1.50
+// string builder
